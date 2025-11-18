ad1cb3ffb35e0306873e1d5ef2b265cad152799d
taehyeon <22101046@handong.ac.kr>
2025-11-18 19:55:05 +0900

    D: add logResult feature to save calculations to file

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include <stdlib.h>
+void logResult(int x, int y, int result);
 
 
 void performCalculation(int x, int y);
@@ -15,6 +16,7 @@ void performCalculation(int x, int y) {
     printf("Calculating result for %d and %d...\n", x, y);
     int result = x + y;
     displayResult(result);
+    logResult(x,y,result); //result의 로깅함 (결과를 파일에 기록)- 4번째 기능 추가. 
 }
 void handleUserInput() {
     int a, b;
@@ -24,4 +26,16 @@ void handleUserInput() {
 }
 void displayResult(int result) {
     printf("The result is: %d\n", result);
+}
+
+void logResult(int x, int y, int result) {
+    FILE *fp = fopen("calc_log.txt", "a");   // 결과 누적 기록
+    if (fp == NULL) {
+        printf("Error: Could not open log file.\n");
+        return;
+    }
+
+    fprintf(fp, "%d + %d = %d\n", x, y, result);
+
+    fclose(fp);
 }
